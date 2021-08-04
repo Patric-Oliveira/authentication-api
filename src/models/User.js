208fcccca240165ae62b57310cc83040147bd971
Patric Oliveira <patric.eng.computation@gmail.com>
2021-08-04 20:51:45 -0300

    added User.js

---
+++ src/models/User.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        lowercase: true,
+        trim: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        select: false,
+        trim: true,
+    },
+}, {timestamps: true}); 
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
