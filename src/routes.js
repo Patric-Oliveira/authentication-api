149fa749a53f9b2808a1c8e795446b92b3fa5989
Patric Oliveira <patric.eng.computation@gmail.com>
2021-08-07 12:34:57 -0300

    update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,15 @@
 const router = require('express').Router();
 
 const HomeController = require('./controllers/HomeController');
+const UserController = require('./controllers/UserController')
+
+// Rota User
+router
+    .get('/register', UserController.index)
+    .get('/register/:id', UserController.show)
+    .post('/register', UserController.store)
+    .put('/register/:id', UserController.update)
+    .delete('/register/id', UserController.delete);
 
 
 // Rota Home
